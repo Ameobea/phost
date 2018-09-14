00ee0353034d0d0ccc115f5dd692979535f76565
Casey Primozic <me@ameo.link>
2018-09-13 20:41:15 -0700

    Update apache2 configs for proper behavior
     * Made everything mesh nicely between the outer proxy (for the wildcard SSL for the subdomains) and the inner proxy (routing between the requests made to deployments, requests made to specific versions of deployments, and requests made to the API server)
     * Added `localhost` to the allowed hosts so that the proxy accepts requests

--- server/server/settings.py
+++ server/server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = ")22)a7uoo6b)_diu(01&c=jdkvv(fby#^1j#yl-laif#k&!=3n"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ameo.design"]
+ALLOWED_HOSTS = ['localhost:7645', 'localhost']
 
 
 # Application definition
