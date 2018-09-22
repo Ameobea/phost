00894e225577b244e93ab58f1da13f5ede9c1a51
Casey Primozic <me@ameo.link>
2018-09-22 02:14:31 -0500

    Allow deployment creation without categories

--- server/serversite/forms.py
+++ server/serversite/forms.py
@@ -6,4 +6,4 @@ class StaticDeploymentForm(forms.Form):
     subdomain = forms.CharField(max_length=255)
     version = forms.CharField(max_length=32)
     file = forms.FileField(allow_empty_file=False)
-    categories = forms.CharField()  # Comma-separated list of categories
+    categories = forms.CharField(required=False)  # Comma-separated list of categories
