48877ceb2c1235877e82a13473e7b55419933480
Casey Primozic <me@ameo.link>
2018-11-20 20:15:32 -0800

    Add script to renew LetsEncrypt certificate
     * Runs a Docker container to execute certbot and authenticate with DigitalOcean in order to generate a new wildcard certificate for the hosting domain

---
+++ server/renew-cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run -it --rm --name certbot-phost \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -v "${PWD}:/root" \
+  certbot/dns-digitalocean certonly --dns-digitalocean \
+    --dns-digitalocean-credentials /root/digitalocean_creds.ini \
+    --cert-name ameo.design \
+    -d *.ameo.design \
+    -d ameo.design
+
+service apache2 restart
